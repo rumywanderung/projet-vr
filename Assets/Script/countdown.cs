d2d1f271db855bea8b03652f6f5189b6ab062a0e
Rumy <rbourkov@gmail.com>
2019-09-04 10:55:13 +0200

    First ver

---
+++ Assets/Script/countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class countdown : MonoBehaviour
+{
+    public float timeLeft;
+
+    #region TIMER
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0)
+        {
+            Debug.Log("CHANGE ROOMS");
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Script/deposable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deposable : MonoBehaviour
+{
+    private int points = 0;
+   
+
+    #region COLLISION
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (this.tag == "deposer" && collision.gameObject.tag == "deposer")
+        {
+            points += 5;
+            Debug.Log(points);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (this.tag == "deposer" && collision.gameObject.tag == "deposer")
+        {
+            points -= 5;
+            Debug.Log(points);
+        }
+    }
+
+    #endregion
+}
